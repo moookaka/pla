1cc1cceb4bda55f2a57e82067a1739e1ac882fe9
moookaka <moehana09@gmail.com>
2025-01-07 14:37:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,36 @@
-const CACHE_NAME = 'plank-timer-v1';
-const assets = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png',
+const CACHE_NAME = "plank-timer-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/manifest.json",
+    "/audio/beep-07.wav",
 ];
 
-// インストール
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(assets);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-// リソース取得
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
