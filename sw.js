af80d8a9846979c5baf7ffd9604079cf241d8258
moookaka <moehana09@gmail.com>
2025-01-07 22:00:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,38 @@
-const CACHE_NAME = "plank-timer-v1";
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/manifest.json",
-    "/audio/beep-07.wav",
-];
 
+const CACHE_NAME = "plank-timer-cache-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./manifest.json",
+    "https://www.soundjay.com/button/beep-07.wav"
+];
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
+            return cache.addAll(urlsToCache);
         })
     );
 });
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
-
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
+    );
+});
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
     );
 });
